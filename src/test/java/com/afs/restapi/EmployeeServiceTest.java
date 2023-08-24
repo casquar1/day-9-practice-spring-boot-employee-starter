3902ba127d9ae13ad3135718f0c27e4c4db762b4
Arianne <chiescake11@gmail.com>
2023-08-24 19:27:42 +0800

    test: implement should_throw_exception_when_create_given_employee_service_and_employee_whose_age_is_less_than_18 or more than 65

--- src/test/java/com/afs/restapi/EmployeeServiceTest.java
+++ src/test/java/com/afs/restapi/EmployeeServiceTest.java
@@ -1,5 +1,6 @@
 package com.afs.restapi;
 
+import com.afs.restapi.exception.EmployeeCreateException;
 import com.afs.restapi.repository.EmployeeJpaRepository;
 import com.afs.restapi.service.EmployeeService;
 import com.afs.restapi.entity.Employee;
@@ -39,6 +40,31 @@ public class EmployeeServiceTest {
         assertEquals(23, employeeResponse.getAge());
         assertEquals("Female", employeeResponse.getGender());
         assertEquals(5000, employeeResponse.getSalary());
+    }
+
+    @Test
+    void should_throw_exception_when_create_given_employee_service_and_employee_whose_age_is_less_than_18() {
+        //given
+        Employee employee = new Employee(1L, "Kate", 16, "Female", 5000);
+
+        //when
+        EmployeeCreateException employeeCreateException = assertThrows(EmployeeCreateException.class, () ->
+                employeeService.create(employee));
+
+        //then
+        assertEquals("Employee must be 18-65 years old", employeeCreateException.getMessage());
+    }
+
+    @Test
+    void should_throw_exception_when_create_given_employee_service_and_employee_whose_age_is_more_than_65() {
+        //given
+        Employee employee = new Employee(1L, "Kate", 66 , "Female", 5000);
+        //when
+        EmployeeCreateException employeeCreateException = assertThrows(EmployeeCreateException.class, () ->
+                employeeService.create(employee));
+
+        //then
+        assertEquals("Employee must be 18-65 years old", employeeCreateException.getMessage());
     }
 }
 
